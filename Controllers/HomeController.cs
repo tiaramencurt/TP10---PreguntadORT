1e4c8fa5d235b9ad418d7a5f7fbac3a4bbe8193f
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-09-26 14:24:00 -0300

    Comentario

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP10.Models;
+
+namespace TP10.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
