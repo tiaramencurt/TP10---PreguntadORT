a2c5760f199c56425d391a0cca53b02d625ca9c0
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-10-03 14:18:19 -0300

    mmmmmmmmm

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,9 +10,13 @@ namespace TP10.Controllers
         {
             string juegoJson = HttpContext.Session.GetString("juego");
             if (string.IsNullOrEmpty(juegoJson))
+            {
                 return new Juego();
+            }
             else
-                return Objeto.StringToObject<Juego>(juegoJson);
+                {
+                    return Objeto.StringToObject<Juego>(juegoJson);
+                }
         }
 
         private void GuardarJuegoEnSession(Juego juego)
@@ -36,33 +40,28 @@ namespace TP10.Controllers
             Juego juego = new Juego();
             juego.CargarPartida(username, categoria);
             GuardarJuegoEnSession(juego);
-
             ViewBag.Username = juego.Username;
             ViewBag.PuntajeActual = juego.PuntajeActual;
             ViewBag.ContadorPregunta = juego.ContadorNroPreguntaActual;
             ViewBag.PreguntaActual = juego.PreguntaActual;
             ViewBag.Respuestas = juego.ListaRespuestas;
-
             return View("Juego");
         }
 
         public IActionResult Jugar()
         {
             Juego juego = ObtenerJuegoDeSession();
-            if (juego.ContadorNroPreguntaActual >= juego.ListaPreguntas.Count)
+            if (juego.ContadorNroPreguntaActual >= juego.ListaPreguntas.Count){
                 return RedirectToAction("Fin");
-
+            }
             juego.PreguntaActual = juego.TraerProximaPregunta();
             juego.ListaRespuestas = juego.TraerProximasRespuestas(juego.PreguntaActual.IdPregunta);
-
             GuardarJuegoEnSession(juego);
-
             ViewBag.Username = juego.Username;
             ViewBag.PuntajeActual = juego.PuntajeActual;
             ViewBag.ContadorPregunta = juego.ContadorNroPreguntaActual;
             ViewBag.PreguntaActual = juego.PreguntaActual;
             ViewBag.Respuestas = juego.ListaRespuestas;
-
             return View("Juego");
         }
 
@@ -70,13 +69,8 @@ namespace TP10.Controllers
         public IActionResult VerificarRespuesta(int idPregunta, int idRespuesta)
         {
             Juego juego = ObtenerJuegoDeSession();
-
-            // Guardar la lista de respuestas antes de avanzar
             List<Respuesta> respuestasDeLaPregunta = new List<Respuesta>(juego.ListaRespuestas);
-
             bool correcta = juego.VerificarRespuesta(idRespuesta);
-
-            // Buscar la respuesta correcta en la lista guardada
             Respuesta respuestaCorrecta = null;
             foreach (Respuesta r in respuestasDeLaPregunta)
             {
@@ -86,28 +80,28 @@ namespace TP10.Controllers
                     break;
                 }
             }
-
             if (correcta)
+            {
                 ViewBag.MensajeRespuesta = "¡Correcto!";
-            else if (respuestaCorrecta != null)
+            }else if (respuestaCorrecta != null)
+            {
                 ViewBag.MensajeRespuesta = "Incorrecto. La correcta era: " + respuestaCorrecta.Opcion + ". " + respuestaCorrecta.Contenido;
+            }
             else
-                ViewBag.MensajeRespuesta = "Incorrecto.";
-
+                {
+                    ViewBag.MensajeRespuesta = "Incorrecto.";
+                }
             GuardarJuegoEnSession(juego);
-
-            if (juego.ContadorNroPreguntaActual >= juego.ListaPreguntas.Count)
+            if (juego.ContadorNroPreguntaActual >= juego.ListaPreguntas.Count){
                 return RedirectToAction("Fin");
-
+            }
             juego.PreguntaActual = juego.TraerProximaPregunta();
             juego.ListaRespuestas = juego.TraerProximasRespuestas(juego.PreguntaActual.IdPregunta);
-
             ViewBag.Username = juego.Username;
             ViewBag.PuntajeActual = juego.PuntajeActual;
             ViewBag.ContadorPregunta = juego.ContadorNroPreguntaActual;
             ViewBag.PreguntaActual = juego.PreguntaActual;
             ViewBag.Respuestas = juego.ListaRespuestas;
-
             return View("Juego");
         }
 
@@ -116,7 +110,7 @@ namespace TP10.Controllers
             Juego juego = ObtenerJuegoDeSession();
             ViewBag.Username = juego.Username;
             ViewBag.PuntajeFinal = juego.PuntajeActual;
-            HttpContext.Session.Remove("juego"); // Limpia la partida
+            HttpContext.Session.Remove("juego");
             return View();
         }
     }
